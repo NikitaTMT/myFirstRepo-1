5da59a0d63c98043df4db7112212950eb5bba0e9
garnio1987 <olegar2004@mail.ru>
2021-03-28 19:46:48 +0300

    Addition html and js
    Addition of text in html and array of persons in js.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 alert("Hello!");
+
+const person = [
+  {
+    name: "German",
+    age: 19,
+    isDeveloper: true,
+    skills: ["React"]
+  },
+  {
+    name: "Alan",
+    age: 15,
+    isDeveloper: true,
+    skills: ["html", "css", "Node Js", "React"]
+  },
+  {
+    name: "Travis",
+    age: 40,
+    isDeveloper: true,
+    skills: []
+  },
+  {
+    name: "Dayzi",
+    age: 33,
+    isDeveloper: false,
+    skills: []
+  },
+  {
+    name: "Rich",
+    age: 22,
+    isDeveloper: false,
+    skills: []
+  }
+];
